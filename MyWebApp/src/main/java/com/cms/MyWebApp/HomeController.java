2ebe729b6f5a96a6ab588ac5d1d3aeb27a450eac
chandramoulisharma <cmsharma.cs@gmail.com>
2020-12-11 17:19:34 +0530

    Explorer Spring boot with more details

--- MyWebApp/src/main/java/com/cms/MyWebApp/HomeController.java
+++ MyWebApp/src/main/java/com/cms/MyWebApp/HomeController.java
@@ -1,7 +1,13 @@
 package com.cms.MyWebApp;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -10,5 +16,31 @@ public class HomeController {
 	public String home() {
 		System.out.println("Home Controller");
 		return "home"; 
+	}
+	
+	@RequestMapping("login")
+	public String exampleWithRequestAndResponse(HttpServletRequest request, HttpServletResponse response) {
+		String name = request.getParameter("name");
+		System.out.println("name ::"+name);
+		HttpSession session = request.getSession();
+		session.setAttribute("name", name);
+		return "login";
+	}
+	
+	@RequestMapping("view")
+	public String example(@RequestParam("name") String myName, HttpSession session) {
+		
+		System.out.println("name ::"+myName);
+		session.setAttribute("name", myName);
+		return "view";
+	}
+	
+	@RequestMapping("newPage")
+	public ModelAndView newPage(@RequestParam("name") String myName ) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("newPage");
+		
+		return mv;
 	}
 }
